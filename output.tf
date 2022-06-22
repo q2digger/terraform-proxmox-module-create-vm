1a00e73de80fb1b2fc097fb95e45d9b67ff36374
Dmitry Gerasimov <>
2022-06-22 22:07:04 +0300

    add output block

---
+++ output.tf
@@ -0,0 +1,18 @@
+#output "instance_ip_address" {
+#  value = proxmox_vm_qemu.test_server.*.default_ipv4_address
+#}
+
+#output "instance_name" {
+#  value = proxmox_vm_qemu.test_server.*.name
+#}
+
+# check some examples
+# https://www.bogotobogo.com/DevOps/Terraform/Terraform-Introduction-AWS-loops.php
+
+output "instance_name_and_ip" {
+  value = {
+    for instance in proxmox_vm_qemu.vm-server:
+      instance.name => instance.default_ipv4_address
+  }
+}
+
